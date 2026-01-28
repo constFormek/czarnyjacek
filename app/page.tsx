90560dcc6321f08ebc11ac82709f5dbf0a39880f
plexi <plexify.code@gmail.com>
2026-01-28 19:03:29 +0100

    blablable

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 import Game from "@/components/Game";
-import { Suspense } from "react";
+
 
 export default function Home() {
   return (
-    <Suspense fallback={<div>game loading...</div>}>
-      <Game />
-    </Suspense>
+    <Game />
   );
 }
